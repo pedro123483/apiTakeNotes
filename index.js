b2a5917ce70e5fa8e7ff4ada8641b6689c381ec9
pedro123483 <pg308758@gmail.com>
2023-01-11 23:46:49 -0300

    lalalalal

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ connectToDatabase();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    next();
+});
+
 app.use("/user", userRoute);
 
 app.listen(port, () => {
