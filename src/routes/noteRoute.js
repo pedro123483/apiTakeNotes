4873db85feb5d5c79ce4cb16aa7f0c1cc6feb525
pedro123483 <pg308758@gmail.com>
2023-01-16 23:30:13 -0300

    allal

--- src/routes/noteRoute.js
+++ src/routes/noteRoute.js
@@ -5,6 +5,6 @@ const router = Router();
 
 router.post("/", noteController.create);
 router.get("/", noteController.getAll);
-router.get("/:userId", noteController.getByUserId);
+router.get("/byUser/:userId", noteController.getByUserId);
 
 export default router;
