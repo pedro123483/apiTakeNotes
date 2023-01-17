cf3629cd50a4f4805846423abff6d7ff2a8545d2
pedro123483 <pg308758@gmail.com>
2023-01-16 22:18:01 -0300

    llallal

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -24,7 +24,7 @@ const create = async (request, response) => {
         const resultExplanation = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `O que é ${title}? Explique detalhadamente.`,
-            temperature: 0,
+            temperature: 0.6,
             max_tokens: 2000
         });
 
